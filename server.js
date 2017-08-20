53cda5edab38e3ec0daa7ea0398c760ce2b177d8
Michal Woloszyn <fraunos@gmail.com>
2017-08-20 22:36:24 +0200

    pierwsze kroki do transformacji w backend wyłącznie

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const Koa = require('koa')
 // const flash = require('koa-flash')
 // const https = require('https')
 const app = new Koa()
-const lastDate = new Date().toLocaleString()
+// const lastDate = new Date().toLocaleString()
 
-app.use(require('koa-static')('static'))
+// app.use(require('koa-static')('static'))
 
 // body parser
 // const bodyParser = require('koa-bodyparser')
@@ -17,71 +17,56 @@ app.use(require('koa-static')('static'))
 const fs = require('fs')
 const router = require('koa-router')()
 
-app.use(router.routes())
+
+
+app.use(async (ctx,next) => {
+  ctx.response.set('Access-Control-Allow-Origin', 'http://localhost:8080');
+  ctx.response.set('Access-Control-Allow-Methods', 'GET');
+  ctx.response.set('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  ctx.response.set('Access-Control-Allow-Credentials', true);
+  await next();
+
+}).use(router.routes())
   .use(router.allowedMethods());
-
-
-blog = fs.readdirSync('./views/blog')
-blogbody = ''
-console.log(blog);
-
-if (blog.length != 0) {
-  for (var i = blog.length - 1; i >= 0; i--) {
-    date = fs.statSync('views/blog/' + blog[i]).mtime.toLocaleString()
-    blog[i] = fs.readFileSync('views/blog/' + blog[i], 'utf8')
-    blogbody += blog[i]
-  }
-}
-
-
-router.get('/', async(ctx) => {
-  ctx.type = 'html'
-  let body = fs.readFileSync('views/blog.html', 'utf8')
-  ctx.body = body.replace('{blog}', blogbody)
-})
+//
+// blog = fs.readdirSync('./views/blog')
+// blogbody = ''
+// console.log(blog);
+//
+// if (blog.length != 0) {
+//   for (var i = blog.length - 1; i >= 0; i--) {
+//     date = fs.statSync('views/blog/' + blog[i]).mtime.toLocaleString()
+//     blog[i] = fs.readFileSync('views/blog/' + blog[i], 'utf8')
+//     blogbody += blog[i]
+//   }
+// }
+//
+//
+// router.get('/blog', async(ctx) => {
+//   ctx.type = 'html'
+//   let body = fs.readFileSync('views/blog.html', 'utf8')
+//   ctx.body = body.replace('{blog}', blogbody)
+// })
 
 const db = require('./database.js');
 
-router.get('/dev', async(ctx) => {
+router.get('/dev/projekty', async(ctx) => {
   ctx.type = 'html'
   ctx.body = await db.Project.findAll({
     attributes: {
       exclude: ['deputies']
     }
   })
+  console.log(ctx);
 })
 
-router.get('/projekt', async(ctx) => {
+router.get('/dev/projekty/:druk', async(ctx) => {
   ctx.type = 'html'
-  let body = '';
-  let projects = await db.Project
-    .findAndCountAll({
-      attributes: ['id', 'drukNr']
-    })
-  body += `<p>${projects.count} projektów</p>`
-
-  for (variable of projects.rows) {
-    body += `<a href="/projekt/${variable.drukNr}">${variable.drukNr}</a><br>`
-  }
-
-  ctx.body = body;
-
-})
-
-router.get('/projekt/:druk', async(ctx) => {
-
-  ctx.type = 'html'
-  let body = ''
-
-  project = (await db.Project.findOne({
+  ctx.body = await db.Project.findOne({
     where: {
       drukNr: ctx.params.druk
     }
-  })).get()
-  for (variable in project) {
-    body += `${variable}: ${JSON.stringify(project[variable])}<br>`;
-  }
-  ctx.body = body;
+  })
 
 });
 
