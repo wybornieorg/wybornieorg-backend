c65a238e20bebb3f548f20cb26c79e1bae81a160
Michał Wołoszyn <fraunos@gmail.com>
2017-12-22 03:13:39 +0100

    zmiana loginu bazy danych

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ console.log('Uruchomiono database.js');
 
 const Sequelize = require('sequelize')
 
-var sequelize = new Sequelize('sejmortestdb', 'postgres', '', {
+var sequelize = new Sequelize('sejmortestdb', 'postgres', 'postgres', {
   host: 'localhost',
   dialect: 'postgres',
   timestamps: true,
